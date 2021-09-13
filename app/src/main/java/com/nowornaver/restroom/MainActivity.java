8d8a96f6b253837467abb883b69c2b827c8f1541
= <redalsdn7@naver.com>
2021-09-13 22:58:55 +0900

    * kakao 검색 api 추가예정

--- app/src/main/java/com/nowornaver/restroom/MainActivity.java
+++ app/src/main/java/com/nowornaver/restroom/MainActivity.java
@@ -20,25 +20,40 @@ import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-public class MainActivity extends AppCompatActivity {
+/* MapViewEventListener interface를 구현하는 객체를 MapView 객체에 등록하여
+* 지도 이동/확대/축소, 지도 화면 터치(Single Tap / Double Tap / Long Press) 이벤트를 통보받을 수 있다.
+* */
+public class MainActivity extends AppCompatActivity implements MapView.MapViewEventListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        getMapView();
+
+        // getHashKey();
+    }
+
+    public void getMapView(){
         MapView mapView = new MapView(this);
 
         RelativeLayout mapViewContainer = findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
-        getHashKey();
+
     }
 
+    /**
+     * 애플리케이션 Key 값 가져오는 함수
+     * 2021.09.12 노민우
+     */
     private void getHashKey(){
         PackageInfo packageInfo = null;
         try {
@@ -58,5 +73,67 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
             }
         }
+    }
+
+    /**
+     * MapView가 사용가능 한 상태가 되었음을 알려준다.
+     * onMapViewInitialized()가 호출된 이후에 MapView 객체가 제공하는 지도 조작 API들을 사용할 수 있다.
+     * @param mapView
+     */
+    @Override
+    public void onMapViewInitialized(MapView mapView) {
+
+    }
+
+    /**
+     * 지도 중심 좌표가 이동한 경우 호출된다.
+     * @param mapView 맵뷰
+     * @param mapPoint 맵 좌
+     */
+    @Override
+    public void onMapViewCenterPointMoved(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    // 지도 확대/축소 레벨이 변경된 경우 호출된다.
+    @Override
+    public void onMapViewZoomLevelChanged(MapView mapView, int i) {
+
+    }
+
+    // 사용자가 지도 위를 터치한 경우 호출된다.
+    @Override
+    public void onMapViewSingleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    // 사용자가 지도 위 한 지점을 더블 터치한 경우 호출된다.
+    @Override
+    public void onMapViewDoubleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    // 사용자가 지도 위 한 지점을 길게 누른 경우(long press) 호출된다.
+    @Override
+    public void onMapViewLongPressed(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    // 사용자가 지도 위 한 지점을 길게 누른 경우(long press) 호출된다.
+    @Override
+    public void onMapViewDragStarted(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    // 사용자가 지도 드래그를 끝낸 경우 호출된다.
+    @Override
+    public void onMapViewDragEnded(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    // 사용자가 지도 드래그를 끝낸 경우 호출된다.
+    @Override
+    public void onMapViewMoveFinished(MapView mapView, MapPoint mapPoint) {
+
     }
 }
