6f5cd5baeb32d9aa28850847f48f7015aad61146
= <redalsdn7@naver.com>
2021-09-28 05:11:58 +0900

    * 현재 gps 좌표 내 위치에 표시 (GpsService 추가)  - kakao 검색 api 추가예정 (보류)
     작업 순서
     1. 현재 gps 좌표 가져와 마커를 내위치에 표시하기 (완료)
     2. 공공포털사이트에서 화장실 api를 사용하여 현재 내 주변 화장실 표시 (검토 중)
        - 화장싱 data 를 firebase 를 사용하여 저장후 가저오는 방식 사용
        - data 를 가져와 지도에 표시

---
+++ app/src/main/java/com/nowornaver/restroom/GpsService.java
@@ -0,0 +1,145 @@
+package com.nowornaver.restroom;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+
+/**
+ * Gps 서비스를 위한 클래스
+ * 현재위치를 가져와 주소로 변환처리를 함.
+ */
+public class GpsService extends Service implements LocationListener {
+
+    private final static String TAG = GpsService.class.getName();
+
+    private final Context mContext;
+    private Location location;
+
+    public double latitude; // 위도
+    public double longitude; // 경도
+
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 거리 업데이트
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // gps 받아오는 갱신 시간
+    protected LocationManager locationManager;
+
+    public GpsService(Context context) {
+        this.mContext = context;
+
+    }
+
+    // Location (위치) 가저오는 함수
+    public Location getLocation() {
+        try {
+            // Acquire a reference to the system Location Manager (시스템 위치 관리자에대한 참조 획득)
+            locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
+
+            boolean isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER); // GPS provider(제공자)사용가능 여부
+            boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER); // 네트워크 provider 사용가능 여부
+
+            if (!isGpsEnabled && isNetworkEnabled) {
+                // 사용 가능하지 않을경우
+            } else {
+                // 네트워크 및 GPS 퍼미션 체크하기위한 변
+                int hasFindLocationPermission = ContextCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION);
+                int hasCoarseLocationPermission = ContextCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION);
+
+                if (!(hasFindLocationPermission == PackageManager.PERMISSION_GRANTED && hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED)) {
+                    return null;
+                }
+
+                if (isNetworkEnabled) {
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+
+                    if (locationManager != null) {
+                        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (location == null) {
+                            // location 이 null 일 경우 좌표 가져오기
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                        }
+                    }
+                }
+
+                if (isGpsEnabled) {
+                    if (location == null) {
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                        if (locationManager != null) {
+                            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (location == null) {
+                                // location 이 null 일 경우 좌표 가져오기
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.d(TAG, e.toString());
+        }
+
+        return location;
+    }
+
+    public double getLatitude() {
+
+        if (location != null) {
+            latitude = location.getLatitude();
+        }
+
+        return latitude;
+    }
+
+    public double getLongitude() {
+
+        if (location != null) {
+            longitude = location.getLongitude();
+        }
+
+        return longitude;
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    public void stopUsingGps() {
+        if (locationManager != null){
+            locationManager.removeUpdates(GpsService.this);
+        }
+    }
+}
